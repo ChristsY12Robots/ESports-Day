884666e6c4c7ed3bc184e55b97944729146249a2
Miles Burne <milo@burne.us>
2017-12-04 13:59:17 +0000

    Update user_save.py

--- Source/user_save.py
+++ Source/user_save.py
@@ -31,13 +31,30 @@ class User():
         else:
             return(False)
 
+    def read_file(self, indicate):
+        f = open(str(self.username+".txt"), "r")
+        file = f.read()
+        f.close()
+        if indicate == 1:
+            if len(file) > 1:
+                return(True)
+            else:
+                return(False)
+        else:
+            output = []
+            file = file.split("\n")
+            for x in file:
+                carry = x.split(",")
+                if file[0] == x:
+                    pass
+                else:
+                    output.append(carry)
+            return(output)
+
     def create_file(self):
         f = open(str(self.username+".txt"), "a+") #opening in append mode IMPORTANT
-        file = f.read() #this is really weird, if the file exists it still says there is NOTHING in it?
-        print(file)
         time = str(datetime.datetime.now())
-        if len(file)>0:
-            f.write(("new session at "+str(time)))
+        if self.read_file(1) == True:
             f.close()
         else:
             f.write((str(self.username)+" created "+str(time)+"\n"))
@@ -50,8 +67,6 @@ class User():
         f = open(str(self.username+".txt"), "a+")
         time = str(datetime.datetime.now())
         f.write(str(game)+","+str(score)+","+time+"\n")
-        file = f.read() #WHY WON'T IT READ!!??!!
-        print(file)
         f.close()
         
         
@@ -64,7 +79,7 @@ user = User()
 
 #testing username checking
 print("Checking Usernames example") #spaces are stripped
-usernames = ["MiloIsDaBest!!1  ","Milo","myUsername","kermit","Milo   ", "Mil0","milo",]
+usernames = ["MiloIsDaBest!!1  ","Milo","myUsername","kermit","Milo   ", "Mil0","milo","burnm035.318"]
 for x in usernames:
     if user.check_username(x) == True:
         print(x, "is your real username")
@@ -82,6 +97,14 @@ for x in range(0,10):
     user.append_file(games[randomGame], randomScore)
     print("file written to "+str(x))
 
+#reading the file
+
+file = user.read_file(0)
+for x in range(0, len(file)-1):
+    game = file[x][0]
+    score = file[x][1]
+    print(user.get_username()+" played "+game+" and got "+score)
+        
 
 
 
